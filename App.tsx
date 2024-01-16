07cc231c714f42b895341ac637e966ce30af046b
Steven Harding <sharding1012@gmail.com>
2024-01-15 19:43:01 -0800

    add initial onboarding component and alias for assets

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
+
+import HomePreviewScreen from './app/features/onboarding/presentation/HomePreviewScreen';
 
 export default function App() {
   return (
     <View style={styles.container} testID="container">
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <HomePreviewScreen />
     </View>
   );
 }
