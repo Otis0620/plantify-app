d4d8be940c054e8fc9a3824f1388680174aaf9c9
Steven Harding <sharding1012@gmail.com>
2024-01-23 23:39:55 -0800

    update PreviewImage storybook controls

--- app/features/onboarding/presentation/components/PreviewImage/PreviewImage.stories.tsx
+++ app/features/onboarding/presentation/components/PreviewImage/PreviewImage.stories.tsx
@@ -1,14 +1,33 @@
 import { Meta, StoryObj } from '@storybook/react-native';
 import HomeScreenPreviewImage from '@assets/onboarding/home-screen-preview.png';
+import DiagnoseScreenPreviewImage from '@assets/onboarding/diagnose-screen-preview.png';
+import MyPlantsScreenPreviewImage from '@assets/onboarding/my-plants-screen-preview.png';
 
 import PreviewImage from './PreviewImage';
+
+const imageSources = {
+  HomeScreenPreviewImage,
+  DiagnoseScreenPreviewImage,
+  MyPlantsScreenPreviewImage,
+};
 
 const meta: Meta<typeof PreviewImage> = {
   title: 'PreviewImage',
   component: PreviewImage,
-  parameters: {
-    notes: `This component uses a static image for presentation purposes, 
-    therefore it is not possible to change the image source.`,
+  argTypes: {
+    imageSource: {
+      defaultValue: 'HomeScreenPreviewImage',
+      mapping: imageSources,
+      options: Object.keys(imageSources),
+      control: {
+        type: 'select',
+        labels: {
+          HomeScreenPreviewImage: 'Home Screen',
+          DiagnoseScreenPreviewImage: 'Diagnose Screen',
+          MyPlantsScreenPreviewImage: 'My Plants Screen',
+        },
+      },
+    },
   },
 };
 
@@ -16,8 +35,4 @@ export default meta;
 
 type Story = StoryObj<typeof PreviewImage>;
 
-export const Primary: Story = {
-  args: {
-    imageSource: HomeScreenPreviewImage,
-  },
-};
+export const Primary: Story = {};
