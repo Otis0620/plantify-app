95bb88a81ec5967881efcdb9d64d8dee59421403
Steven Harding <sharding1012@gmail.com>
2024-01-28 23:49:10 -0800

    add storybook decorator to handle navigation in stories

--- app/features/onboarding/presentation/components/Buttons/PreviewButtonGroup/PreviewButtonGroup.stories.tsx
+++ app/features/onboarding/presentation/components/Buttons/PreviewButtonGroup/PreviewButtonGroup.stories.tsx
@@ -1,14 +1,21 @@
 import { Meta, StoryObj } from '@storybook/react-native';
 
+import NavigationDecorator from '~storybook/decorators/NavigationDecorator';
+
 import PreviewButtonGroup from './PreviewButtonGroup';
 
 const meta: Meta<typeof PreviewButtonGroup> = {
   title: 'PreviewButtonGroup',
   component: PreviewButtonGroup,
+  decorators: [NavigationDecorator],
 };
 
 export default meta;
 
 type Story = StoryObj<typeof PreviewButtonGroup>;
 
-export const Primary: Story = {};
+export const Primary: Story = {
+  args: {
+    navigateTo: '',
+  },
+};
